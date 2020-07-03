45ae7585851d37d52b96b31711349a0a8e60ed07
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 17:20:39 +0530

    Adding getProduct method to know price of each product

--- PromotionEngine/PromotionEngine/SKUProduct.cs
+++ PromotionEngine/PromotionEngine/SKUProduct.cs
@@ -14,7 +14,35 @@ namespace PromotionEngine
         public SKUProduct()
         {
         }
-        
+
+        public SKUProduct getProduct(string Productid)
+        {
+            SKUProduct product = new SKUProduct();            
+            switch (Productid)
+            {
+                case "A":
+                    product.Id = Productid;
+                    product.Price = 50;                    
+
+                    break;
+                case "B":
+                    product.Id = Productid;
+                    product.Price = 30;                    
+
+                    break;
+                case "C":
+                    product.Id = Productid;
+                    product.Price = 20;                    
+
+                    break;
+                case "D":
+                    product.Id = Productid;
+                    product.Price = 15;                    
+                    break;
+            }
+            return product;
+        }
+
     }
 }
 
