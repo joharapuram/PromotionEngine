9ffcb24e9bafc5364eb751f99728f0dd0aef7b37
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 17:14:00 +0530

    Adding SKUProduct.cs file for adding properties

---
+++ PromotionEngine/PromotionEngine/SKUProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class SKUProduct
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+        public SKUProduct()
+        {
+        }
+        
+    }
+}
+
