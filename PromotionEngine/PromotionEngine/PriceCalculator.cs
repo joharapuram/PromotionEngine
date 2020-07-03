c2c1ba49d40fde43b9e4f7238e9bb6ef480cb9f3
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 17:42:52 +0530

    Adding Products to List

---
+++ PromotionEngine/PromotionEngine/PriceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class PriceCalculator
+    {
+        public int GetTotalPrice(List<SKUProduct> products)
+        {
+            return 0;
+        }
+    }
+}
