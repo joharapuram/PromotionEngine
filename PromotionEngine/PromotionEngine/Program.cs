0bfa795fd9c4e619369ae8f2dbbd656c75a2d691
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 17:53:57 +0530

    Reading Number of Orders from Console and Displaying Products Price

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -13,20 +13,17 @@ namespace PromotionEngine
             SKUProduct sKUProduct = new SKUProduct();
             List<SKUProduct> listofProducts = new List<SKUProduct>();
 
-            //A SKU
-            Console.WriteLine("Please enter type of sku product A");
-            string skuType = Console.ReadLine();
-            SKUProduct product = sKUProduct.getProduct(skuType.ToUpper());
-            listofProducts.Add(product);
-            Console.WriteLine("Price of Product {0} is {1}", listofProducts[0].Id, listofProducts[0].Price);
+            Console.WriteLine("Enter total number of order");
+            int numberOfUnits = Convert.ToInt32(Console.ReadLine());
 
-            //B SKU
-            Console.WriteLine("Please enter type of sku product B");
-            string skuTypeB = Console.ReadLine();
-            SKUProduct productB = sKUProduct.getProduct(skuTypeB.ToUpper());
-            listofProducts.Add(productB);
-            Console.WriteLine("Price of Product {0} is {1}", listofProducts[1].Id, listofProducts[1].Price);
-            Console.ReadLine();
+            for (int i = 0; i < numberOfUnits; i++)
+            {
+                Console.WriteLine("Please enter type of sku product A,B,C,D");
+                string skuType = Console.ReadLine();
+                SKUProduct product = sKUProduct.getProduct(skuType.ToUpper());
+                listofProducts.Add(product);
+                Console.WriteLine("Price of Product {0} is {1}", listofProducts[i].Id, listofProducts[i].Price);
+            }            
         }
     }
 }
