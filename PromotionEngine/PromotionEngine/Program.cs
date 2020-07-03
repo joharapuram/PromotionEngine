946694d24778edff788690ba5d899f9455109b93
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 17:46:32 +0530

    Reading Product Names from Console window

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -11,9 +11,21 @@ namespace PromotionEngine
         static void Main(string[] args)
         {
             SKUProduct sKUProduct = new SKUProduct();
-            string skuType = "A";
+            List<SKUProduct> listofProducts = new List<SKUProduct>();
+
+            //A SKU
+            Console.WriteLine("Please enter type of sku product A");
+            string skuType = Console.ReadLine();
             SKUProduct product = sKUProduct.getProduct(skuType.ToUpper());
-            Console.WriteLine("Price of Product {0} is {1}", product.Id, product.Price);
+            listofProducts.Add(product);
+            Console.WriteLine("Price of Product {0} is {1}", listofProducts[0].Id, listofProducts[0].Price);
+
+            //B SKU
+            Console.WriteLine("Please enter type of sku product B");
+            string skuTypeB = Console.ReadLine();
+            SKUProduct productB = sKUProduct.getProduct(skuTypeB.ToUpper());
+            listofProducts.Add(productB);
+            Console.WriteLine("Price of Product {0} is {1}", listofProducts[1].Id, listofProducts[1].Price);
             Console.ReadLine();
         }
     }
