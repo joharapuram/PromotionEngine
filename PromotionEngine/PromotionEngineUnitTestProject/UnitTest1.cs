70462cedc82ac40fd78fb47ce0353375aa8c4a35
Nettikantaiah Joharapuram <njoharapuram@firstam.com>
2020-07-03 18:48:33 +0530

    Unittesting: testing C and D promotion and without A nd B promotion

--- PromotionEngine/PromotionEngineUnitTestProject/UnitTest1.cs
+++ PromotionEngine/PromotionEngineUnitTestProject/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PromotionEngine;
 
@@ -8,12 +9,27 @@ namespace PromotionEngineUnitTestProject
     public class UnitTest1
     {
         SKUProduct sKUProduct = new SKUProduct();
+        PriceCalculator priceCalculator = new PriceCalculator();
+
         [TestMethod]
         public void getProductPrice()
         {
             string Id = "a";
             SKUProduct product = sKUProduct.getProduct(Id.ToUpper());
             Assert.AreEqual(50, product.Price);
+        }
+
+        [TestMethod]
+        public void GetTotalCostWithCandDSKUPromotion()
+        {
+            List<SKUProduct> listofProducts = new List<SKUProduct>();
+            listofProducts.Add(new SKUProduct() { Id = "A", Price = 50 });
+            listofProducts.Add(new SKUProduct() { Id = "B", Price = 30 });
+            listofProducts.Add(new SKUProduct() { Id = "C", Price = 20 });
+            listofProducts.Add(new SKUProduct() { Id = "D", Price = 15 });
+           
+            int TotalCost = priceCalculator.GetTotalPrice(listofProducts);
+            Assert.AreEqual(TotalCost, 110);
         }
     }
 }
